8ec1fca870b01e401b5192645249a77959970af3
arhamCh24 <arhammu@sheridancollege.ca>
2025-09-17 17:15:40 -0400

    added variables, getter, and setters in the parttime

---
+++ src/studentlist/Parttime.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author muhammadarham
+ */
+public class Parttime {
+    private int sid;
+    private int nofhrs;
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public int getNofhrs() {
+        return nofhrs;
+    }
+
+    public void setNofhrs(int nofhrs) {
+        this.nofhrs = nofhrs;
+    }
+    
+}
